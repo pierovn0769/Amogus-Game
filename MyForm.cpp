6607a74e190b80a2a46ce8bc62eedd47ea8e5e3c
Piero Vera <83783791+pierovn0769@users.noreply.github.com>
2021-12-03 14:31:55 -0500

    Create MyForm.cpp

---
+++ MyForm.cpp
@@ -0,0 +1,8 @@
+#include "MyForm.h"
+using namespace Ejercicio011;
+int main() {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::Run(gcnew MyForm());
+	return 0;
+}
